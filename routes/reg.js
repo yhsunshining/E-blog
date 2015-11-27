b24fe04a852d61529d354d278919278dfb9c9419
yhsunshining <yhsunshining@gmail.com>
2015-11-27 19:41:53 +0800

    reg router

--- routes/reg.js
+++ routes/reg.js
@@ -1,3 +1,4 @@
+"use strict";
 var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
@@ -9,15 +10,39 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res, next) {
 	console.log(req.body)
-	var name = req.body.name,
+	var name = req.body.username,
 		password = req.body.password,
 		repassword = req.body['password-repeat'];
 	if(repassword!=password){
 		req.flash('error',"psw not eaque");
 		return res.redirect('/reg');
 	}
+	var  md5 = crypto.createHash('md5');
+	var password = md5.update(req.body.password).digest('hex');
+	var user = new User({
+		name:name,
+		password:password
+	});
 	
-	res.redirect('/');
+	User.get(user.name,function(err,user){
+		if(err){
+			req.flash('error',err);
+			return res.redirect('/');
+		}
+		if(user){
+			req.flash('error','user exist');
+			return res.redirect('/');
+		}
+		user.save(function(err,user){
+			if(err){
+				req.flash('error',err);
+				return res.redirect('/reg');
+			}
+			req.session.user = user;
+			req.flash('success','reg success');
+			res.redirect('/');
+		})
+	})
 });
 
 module.exports = router;
